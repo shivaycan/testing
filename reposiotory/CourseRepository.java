d66b7c01be74f004656d74df171a7cae8a056316
ShivayGarg <121811160+shivaycan@users.noreply.github.com>
2025-02-14 17:47:07 +0530

    Update CourseRepository.java

--- reposiotory/CourseRepository.java
+++ reposiotory/CourseRepository.java
@@ -1,5 +1,12 @@
-//package com.example.shiksha.reposiotory;
-//
-//public class CourseRepository {
-//
-//}
+package com.example.shiksha.reposiotory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.shiksha.model.Course;
+import com.example.shiksha.model.User;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long>{
+	
+}

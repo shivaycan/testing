e935ac629cf2f17f488bcb73116941eddf85e0bd
ShivayGarg <121811160+shivaycan@users.noreply.github.com>
2025-02-14 17:47:43 +0530

    Update Course.java

--- model/Course.java
+++ model/Course.java
@@ -1,5 +1,26 @@
-//package com.example.shiksha.model;
-//
-//public class Course {
-//
-//}
+package com.example.shiksha.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Course {
+		
+	@Id
+	@GeneratedValue
+	private Long courseId;
+	
+	
+	private String title;
+	private String description;
+	private String contentURL;
+	
+	
+	@ManyToOne // many courses can be created by one instructor
+	@JoinColumn(name = "instructor_id") // in course table, a new column will be created by the name insructor id, which will store the id of the instructor
+	private User instructor;
+	
+}

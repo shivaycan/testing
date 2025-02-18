7dd194fed82e0b5b5bb9fa0de82659ea0570a9ea
Garg <2381907@cognizant.com>
2025-02-18 10:08:01 +0530

    initial commit acacdmey

---
+++ src/test/java/com/example/shiksha/AcadmeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadmeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

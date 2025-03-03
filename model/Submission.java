932b21b1c0c391bfbca1719b519a6fc6d108dd21
ShivayGarg <121811160+shivaycan@users.noreply.github.com>
2025-03-03 17:30:44 +0530

    Update Submission.java

--- model/Submission.java
+++ model/Submission.java
@@ -1,12 +1,11 @@
-package com.example.shiksha.model;
+package org.example.academy.model;
+
+
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @Entity
@@ -14,33 +13,30 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Submission {
-
     @Id
     @GeneratedValue
     private Long submissionId;
 
-    @ManyToOne // many submission can be submitted by one student
-    @JoinColumn(name="assessment_id",referencedColumnName = "assessmentId")
+    @ManyToOne // many submissions can be submitted by one student
+    @JoinColumn(name = "assessment_id")
     private Assessment assessment;
 
     @ManyToOne
-    @JoinColumn(name="student_id")
+    @JoinColumn(name = "student_id")
     private User student;
 
     @ElementCollection
-    @CollectionTable(name="submitted_answers", joinColumns = @JoinColumn(name="submission_id"))
-    @MapKeyColumn(name="question_id")
-    @Column(name="student_answers")
-    private Map<Long,String> answers;
+    @CollectionTable(name = "submitted_answers", joinColumns = @JoinColumn(name = "submission_id"))
+    @MapKeyColumn(name = "question_id")
+    @Column(name = "student_answers")
+    private Map<Long, String> answers;
 
     private Integer score;
+    private String status; // SUBMITTED or GRADED
 
-    private String status; //SUBMITTED or GRADED
-
-    public Submission(Assessment assessment,User student, Map<Long,String> answers){
+    public Submission(Assessment assessment, User student, Map<Long, String> answers) {
         this.assessment = assessment;
         this.student = student;
         this.answers = answers;
     }
-
 }
